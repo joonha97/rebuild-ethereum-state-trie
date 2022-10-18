0beb63b9d13d6d2e96f0199897d201b0b6c3e43e
joonha97 <kyn04138@naver.com>
2022-10-19 08:29:32 +0900

    Add files via upload

--- statedb.go
+++ statedb.go
@@ -1,4 +1,4 @@
-// RebuildStorageTrieFromKeyValue rebuilds a trie with hard-coded trie (joonha)
+// RebuildStorageTrieFromKeyValue rebuilds a storage trie with hard-coded trie (joonha)
 func (s *StateDB) RebuildStorageTrieFromKeyValue(leveldbPath_in string, trieRoot common.Hash, leveldbPath_out_normal string, leveldbPath_out_stack string) { // state trie
 
 	/* config */
@@ -6,25 +6,22 @@ func (s *StateDB) RebuildStorageTrieFromKeyValue(leveldbPath_in string, trieRoot
 		normalTrie = 1
 		stackTrie  = 2
 	)
-	simulatingTrie := stackTrie
+	simulatingTrie := normalTrie
 
 	/* load key-value */
 	common.LoadAccountsAndKeys()
 	accounts := common.Accounts
 	keys := common.Keys
 
-
 	/* normal trie */
 	if simulatingTrie == normalTrie {
-		
 		// new trie
 		diskdb_out_normal, err := leveldb.New(leveldbPath_out_normal, leveldbCache, leveldbHandles, leveldbNamespace, leveldbReadonly)
 		if err != nil {
 			fmt.Println("leveldb.New error (2) !! ->", err)
 			os.Exit(1)
 		}
-		rebuiltTrie, _ = trie.NewSecure(common.Hash{}, trie.NewDatabase(diskdb_out_normal))
-		
+		normalTr, _ := trie.New(common.Hash{}, trie.NewDatabase(diskdb_out_normal))
 		// inject the data
 		RebuildNormalTrieStarts := time.Now().UnixNano() / int64(time.Millisecond)
 		TrieUpdateStarts := time.Now().UnixNano() / int64(time.Millisecond)
@@ -37,7 +34,7 @@ func (s *StateDB) RebuildStorageTrieFromKeyValue(leveldbPath_in string, trieRoot
 			}
 
 			acc, _ := rlp.EncodeToBytes(data)
-			if err := rebuiltTrie.TryUpdate_SetKey(keys[i][:], acc); err != nil {
+			if err := normalTr.TryUpdate(keys[i][:], acc); err != nil {
 				s.setError(fmt.Errorf("updateStateObject (%x) error: %v", addr[:], err))
 			}
 		}
@@ -46,18 +43,17 @@ func (s *StateDB) RebuildStorageTrieFromKeyValue(leveldbPath_in string, trieRoot
 		// trie commit
 		var root common.Hash
 		trieCommitStarts := time.Now().UnixNano() / int64(time.Millisecond)
-		if root, _, err = rebuiltTrie.Commit(nil); err != nil {
-			fmt.Println("rebuiltTrie.Commit error !!")
+		if root, _, err = normalTr.Commit(nil); err != nil {
+			fmt.Println("normalTr.Commit error !!")
 		}
 		trieCommitEnds := time.Now().UnixNano() / int64(time.Millisecond)
 
 		// db commit
 		dbCommitStarts := time.Now().UnixNano() / int64(time.Millisecond)
-		rebuiltTrie.Database().Commit(root, false, nil)
+		normalTr.Database().Commit(root, false, nil)
 		dbCommitEnds := time.Now().UnixNano() / int64(time.Millisecond)
 
 		RebuildNormalTrieEnds := time.Now().UnixNano() / int64(time.Millisecond)
-
 		fmt.Println("$$$ rebuilding the normal trie done")
 		fmt.Println("Update Trie Time Duration: ", TrieUpdateEnds-TrieUpdateStarts, "(ms)")
 		fmt.Println("Trie Commit Time Duration: ", trieCommitEnds-trieCommitStarts, "(ms)")
@@ -82,9 +78,7 @@ func (s *StateDB) RebuildStorageTrieFromKeyValue(leveldbPath_in string, trieRoot
 			stackTr.TryUpdate(key[:], accounts[index])
 		}
 
-		
 		RebuildStackTrieEnds := time.Now().UnixNano() / int64(time.Millisecond)
-		
 		fmt.Println("$$$ rebuilding the stack trie done")
 		fmt.Println("Rebuild Stack Trie Time Duration: ", RebuildStackTrieEnds-RebuildStackTrieStarts, "(ms)")
 		fmt.Println("stackTr.Hash(): ", stackTr.Hash())
